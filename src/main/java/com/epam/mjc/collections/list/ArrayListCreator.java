836a47f2e895e8c316280ec2da19f8819dd4fdcd
martam00 <marta.michniacka1@gmail.com>
2023-08-02 16:11:32 +0200

    lists

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> returnList = new ArrayList<>();
+
+        int i = 1;
+        for (String s : sourceList) {
+            if (i % 3 == 0) {
+                returnList.add(s);
+                returnList.add(s);
+
+            }
+            i++;
+        }
+        return returnList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,39 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+
+        ListComparator comparator = new ListComparator();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            for (int j = 1; j < sourceList.size(); j++) {
+                int smaller = comparator.compare(sourceList.get(j - 1), sourceList.get(j));
+                if (smaller == -1) {
+                    String temp = sourceList.get(j -1);
+                    sourceList.set(j - 1, sourceList.get(j));
+                    sourceList.set(j, temp);
+                }
+            }
+        }
+
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        //`5x^2+3`
+        int intA = 5 * (int) Math.pow(Integer.parseInt(a), 2) + 3;
+        int intB = 5 * (int) Math.pow(Integer.parseInt(b), 2) + 3;
+        if (intA < intB) {
+            return 1;
+        } else {
+            return -1;
+        }
+
     }
 }
